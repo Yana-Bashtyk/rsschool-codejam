8a91e9de69c8cace9acc989218829589372ef518
Yana Bashtyk <yankabashtyk16@gmail.com>
2018-10-28 19:03:36 +0300

    refactor: add module exports and initial value to the function

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,4 +1,4 @@
-function sumOfOther(arr) {
-  const arrSum = arr.reduce((a, b) => a + b);
+module.exports = function sumOfOther(arr) {
+  const arrSum = arr.reduce((a, b) => (a + b), 0);
   return arr.map(elem => arrSum - elem);
-}
+};
