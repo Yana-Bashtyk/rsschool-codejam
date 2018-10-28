e4614531cf3db0ce7b0d5eeab9d5de7051508a3c
Yana Bashtyk <yankabashtyk16@gmail.com>
2018-10-28 20:42:24 +0300

    docs: add packages

--- src/recursion.js
+++ src/recursion.js
@@ -1,3 +1,13 @@
-function recursion () {
-    
-}
+module.exports = function recursion(tree) {
+  const result = [];
+  const arr = [];
+  for (const prop in tree) {
+    const value = tree[prop];
+    if (typeof value === 'object') {
+      arr.push(recursion(value));
+    } else {
+      arr.push(value);
+    }
+  }
+  return result.concat(...arr);
+};
