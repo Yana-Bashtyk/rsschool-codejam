febc519b44a0d762fb4fbd19fd95b0a748011a77
Yana Bashtyk <yankabashtyk16@gmail.com>
2018-10-28 14:58:16 +0300

    feat: add solution to count arguments

--- src/make.js
+++ src/make.js
@@ -1,3 +1,13 @@
-function make() {
-    
+function make(...theArgs) {
+  let callbacks = Array.from(theArgs);
+  let tmp = [];
+  return function inner(...args) {
+    for (let i = 0; i < args.length; i += 1) {
+      if (typeof args[i] !== 'function') {
+        tmp = Array.from(args);
+        callbacks = callbacks.concat(tmp);
+        return inner;
+      } return callbacks.reduce((a, b) => (args[i](a, b)));
+    }
+  };
 }
